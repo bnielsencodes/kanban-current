bf5d0071c956ac6e49a8e8af4ee5018c8b14f0fd
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-06-05 00:45:03 -0700

    Add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,27 @@
+import type { Metadata } from "next";
 import "./globals.css";
 
+export const metadata: Metadata = {
+  title: "Kanban | Brandon Nielsen",
+  description: "Kanban, a task manager",
+  authors: [{ name: "Brandon Nielsen" }],
+  creator: "Brandon Nielsen",
+  publisher: "Brandon Nielsen",
+  keywords: [
+    "web developer",
+    "code",
+    "web development",
+    "next.js",
+    "nextjs",
+    "react",
+    "javascript",
+    "oregon",
+    "small businesses",
+    "frontend",
+    "front-end",
+    "software engineer",
+    "programmer",
+  ],
 };
 
 export default function RootLayout({
