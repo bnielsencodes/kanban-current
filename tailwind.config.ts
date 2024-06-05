a7b37c35b7f122a75926c2ba91382c5d274bff8a
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-06-05 00:42:20 -0700

    Add responsive screen sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -42,6 +42,16 @@ const config: Config = {
       },
       overlay: "rgba(0, 0, 0, 0.5)",
     },
+    screens: {
+      // => @media (min-width: 640px) { ... }
+      sm: "640px",
+      // => @media (min-width: 768px) { ... }
+      md: "768px",
+      // => @media (min-width: 1024px) { ... }
+      lg: "1024px",
+      // => @media (min-width: 1440px) { ... }
+      xl: "1440px",
+    },
   },
   plugins: [],
 };
