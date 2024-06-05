9bced92af1860fdd919d072563ca9270a9f8a9ee
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-06-05 00:48:38 -0700

    Update server.ts

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,9 +1,11 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-export const createClient = () => {
+export function createClient() {
   const cookieStore = cookies();
 
+  // Create a server's supabase client with newly configured cookie,
+  // which could be used to maintain user's session
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
@@ -31,6 +33,6 @@ export const createClient = () => {
           }
         },
       },
-    },
+    }
   );
-};
+}
