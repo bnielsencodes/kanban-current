5182ce14bc8c07e6c830ea2002e8e2509e6bfa20
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-06-09 19:50:34 -0700

    Create AccountModal.tsx

---
+++ app/_components/modals/account/AccountModal.tsx
@@ -0,0 +1,44 @@
+import React, { FC, useState, useEffect } from "react";
+import clsx from "clsx";
+
+interface AccountModalProps {
+  session: any;
+  darkMode: boolean;
+  showAccountModal: boolean;
+  setShowAccountModal: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const AccountModal: FC<AccountModalProps> = ({
+  session,
+  darkMode,
+  showAccountModal,
+  setShowAccountModal,
+}) => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [full_name, setFullname] = useState<string | null>(null);
+  const [username, setUsername] = useState<string | null>(null);
+  const [avatar_url, setAvatarUrl] = useState<string | null>(null);
+
+  return (
+    <div
+      className={clsx(
+        "absolute left-0 top-16 h-screen w-screen cursor-default select-none",
+        showAccountModal ? "visible opacity-100" : "invisible opacity-0",
+      )}
+    >
+      <div
+        className="absolute -top-[83px] bottom-0 left-0 right-0 bg-overlay"
+      ></div>
+
+      <div
+        className={clsx(
+          "absolute left-1/2 top-1/2 mx-auto w-[calc(100%-32px)] max-w-[352px] -translate-x-1/2 -translate-y-1/2 rounded-md p-6 md:max-w-[480px] md:p-8",
+          darkMode ? "bg-grey-dark text-white" : "bg-white text-grey-dark",
+        )}
+      >
+      </div>
+    </div>
+  );
+};
+
+export default AccountModal;
