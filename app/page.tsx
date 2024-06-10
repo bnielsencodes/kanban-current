36cef0142110ec5984ae4f11022b946cfdc074c7
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-06-09 18:58:44 -0700

    Create and store Supabase session

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,43 @@
+"use client";
+import React, { FC, useState, useEffect } from "react";
+import { createClient } from "@/utils/supabase/client";
+import ViewTaskModal from "./_components/modals/view-task/ViewTaskModal";
+
+interface Session {
+  user: {
+    id: string;
   };
+}
+
+const App: FC = () => {
+  const supabase = createClient();
+  const [session, setSession] = useState<Session | null>(null);
+
+  // get supabase session
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }: any) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event: any, session: Session | null) => {
+      setSession(session);
+    });
+  }, []);
+
 
 
       </div>
+  return (
+    <div
+      className={clsx(
+        "h-screen w-screen",
+        darkMode ? "bg-background-dark" : "bg-background-light",
+      )}
+    >
 
           >
     </div>
   );
+};
+
+export default App;
