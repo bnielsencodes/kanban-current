d069b1bcde870d0d8023b9ec9dbbaa1ce122ea04
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-06-09 20:00:59 -0700

    Import Column and NewColumnButton

--- app/_components/taskboard/states/CurrentBoard.tsx
+++ app/_components/taskboard/states/CurrentBoard.tsx
@@ -1,3 +1,6 @@
+import data from "../../../data";
+import Column from "../Column";
+import NewColumnButton from "../NewColumnButton";
 import EmptyBoard from "./EmptyBoard";
 
 export default function CurrentBoard({
@@ -32,6 +35,20 @@ export default function CurrentBoard({
   return (
     <>
       {currentBoardData.columns.length > 0 ? (
+        <div className="flex h-full w-full items-start justify-start gap-x-4 overflow-y-scroll p-4 md:gap-x-6 md:p-6">
+          <div className="flex items-start justify-start gap-x-4 md:gap-x-6">
+            {currentBoardData.columns.map((column) => {
+              return (
+                <Column
+                  key={column.id}
+                  {...{ column, darkMode, setShowViewTaskModal }}
+                />
+              );
+            })}
+          </div>
+
+          <NewColumnButton {...{ darkMode, setShowEditBoardModal }} />
+        </div>
       ) : (
         <EmptyBoard {...{ setShowEditBoardModal }} />
       )}
