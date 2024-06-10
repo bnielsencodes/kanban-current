1c9100fa0fff9d3b69a852952b81f819bd86bf8e
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-06-09 19:58:47 -0700

    Create CurrentBoard.tsx

---
+++ app/_components/taskboard/states/CurrentBoard.tsx
@@ -0,0 +1,34 @@
+export default function CurrentBoard({
+  darkMode,
+  currentBoardData,
+  setShowEditBoardModal,
+  setShowViewTaskModal,
+}: {
+  darkMode: boolean;
+  currentBoardData: {
+    id: number;
+    name: string;
+    columns: {
+      id: number;
+      name: string;
+      tasks: {
+        id: number;
+        title: string;
+        description: string;
+        status: string;
+        subtasks: {
+          id: number;
+          title: string;
+          isCompleted: boolean;
+        }[];
+      }[];
+    }[];
+  };
+  setShowEditBoardModal: React.Dispatch<React.SetStateAction<boolean>>;
+  setShowViewTaskModal: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  return (
+    <>
+    </>
+  );
+}
