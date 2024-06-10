837b4bef72f5225f76e00512c0953bead5ba8043
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-06-09 19:00:26 -0700

    show loader if not mounted

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ interface Session {
 const App: FC = () => {
   const supabase = createClient();
   const [session, setSession] = useState<Session | null>(null);
+  const [mounted, setMounted] = useState(false);
 
   // get supabase session
   useEffect(() => {
@@ -26,7 +27,24 @@ const App: FC = () => {
 
 
 
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return (
+      <div
+        className={clsx(
+          "flex h-screen w-screen items-center justify-center",
+          darkMode ? "bg-background-dark" : "bg-background-light",
+        )}
+      >
+        {/* loader */}
+        <span className="loader relative h-12 w-12 overflow-hidden rounded-full bg-primary shadow-[0_5px_12px_rgba(0,0,0,.15),inset_0_0_30px_4px_rgba(0,0,0,.15)] before:absolute before:top-[-40%] before:h-full before:w-full before:animate-wave before:rounded-[30%] before:bg-grey-medium before:content-[''] after:absolute after:top-[-40%] after:h-full after:w-full after:animate-wave after:rounded-[45%] after:bg-lines-light after:content-[''] lg:h-16 lg:w-16"></span>
       </div>
+    );
+  }
+
   return (
     <div
       className={clsx(
