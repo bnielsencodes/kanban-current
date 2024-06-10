49bd21378d557630df41cdc67a431e4462a246d1
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-06-09 19:14:13 -0700

    Create Header.tsx

---
+++ app/_components/header/Header.tsx
@@ -0,0 +1,20 @@
+export default function Header({
+  darkMode,
+  showSidebar,
+}: {
+  darkMode: boolean;
+  showSidebar: boolean;
+}) {
+  return (
+    <header
+      className={clsx(
+        "relative flex h-16 items-center justify-between pl-4 md:h-[81px] md:pl-0 lg:h-[96px]",
+        darkMode
+          ? "bg-grey-dark md:border-lines-dark"
+          : "bg-white md:border-lines-light",
+        showSidebar ? "border-b-0" : "md:border-b-[1px]",
+      )}
+    >
+    </header>
+  );
+}
