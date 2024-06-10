3c378e811487bdae25f881187d736f09be8e9fb6
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-06-09 19:06:03 -0700

    Create Auth form

--- app/_components/login/Auth.tsx
+++ app/_components/login/Auth.tsx
@@ -3,6 +3,7 @@ import { createClient } from "@/utils/supabase/client";
 import Image from "next/image";
 import logoLight from "../../../public/assets/logo-light.svg";
 import logoDark from "../../../public/assets/logo-dark.svg";
+import clsx from "clsx";
 
 export default function Auth({ darkMode }: { darkMode: boolean }) {
   const supabase = createClient();
@@ -36,6 +37,36 @@ export default function Auth({ darkMode }: { darkMode: boolean }) {
         width={153}
         height={26}
       />
+      <form className="mt-5 flex flex-col gap-2" onSubmit={handleLogin}>
+        <label
+          className={clsx("", darkMode ? "text-white" : "text-grey-medium")}
+          htmlFor="email"
+        >
+          Sign in via magic link with your email below
+        </label>
+        <input
+          className={clsx(
+            "mb-5 h-10 w-full rounded border-[1px] border-input pl-4 text-[13px] duration-300 focus:border-primary focus:outline-none focus:ring-0 lg:cursor-pointer lg:hover:border-primary",
+            darkMode
+              ? "bg-grey-dark placeholder-light"
+              : "bg-white placeholder-dark",
+          )}
+          id="email"
+          type="email"
+          placeholder="Your email"
+          value={email}
+          required={true}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button
+          className={
+            "block h-12 w-full rounded-3xl border-0 bg-primary duration-300 focus:border-[1px] focus:border-primary-hover focus:outline-none focus:ring-0 lg:hover:bg-primary-hover"
+          }
+          disabled={loading}
+        >
+          {loading ? <span>Loading</span> : <span>Send magic link</span>}
+        </button>
+      </form>
     </div>
   );
 }
