c691cd071b0fa307fc9cad5d29cd93bebfc5b9d6
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-06-09 20:06:54 -0700

    Create EditDeleteBoardModal.tsx

---
+++ app/_components/modals/edit-delete-modal/EditDeleteBoardModal.tsx
@@ -0,0 +1,57 @@
+import clsx from "clsx";
+
+export default function EditDeleteBoardModal({
+  darkMode,
+  showEditDeleteBoardModal,
+  setShowEditDeleteBoardModal,
+  setShowEditBoardModal,
+  setShowDeleteBoardModal,
+}: {
+  darkMode: boolean;
+  showEditDeleteBoardModal: boolean;
+  setShowEditDeleteBoardModal: React.Dispatch<React.SetStateAction<boolean>>;
+  setShowEditBoardModal: React.Dispatch<React.SetStateAction<boolean>>;
+  setShowDeleteBoardModal: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  return (
+    <>
+      {/* overlay */}
+      <div
+        className={clsx(
+          "absolute right-0 top-0 z-30 h-screen w-screen cursor-default select-none",
+          showEditDeleteBoardModal
+            ? "visible opacity-100"
+            : "invisible opacity-0",
+        )}
+        onClick={() => setShowEditDeleteBoardModal(false)}
+      >
+        {/* modal */}
+        <div
+          className={clsx(
+            "edit-delete-board-modal relative z-40 mb-0 ml-auto mr-4 mt-[57px] flex w-[192px] select-none flex-col gap-4 rounded-lg pb-[17px] pl-[17px] pt-[17px] md:mr-6 md:mt-[75px] lg:mt-[90px]",
+            darkMode ? "bg-background-dark" : "bg-white",
+            showEditDeleteBoardModal
+              ? "visible translate-y-0 opacity-100"
+              : "invisible -translate-y-2/4 opacity-0",
+          )}
+        >
+          {/* show EDIT board modal button */}
+          <button
+            className="text-left text-[13px] leading-[23px] text-grey-medium hover:text-opacity-80"
+            onClick={() => setShowEditBoardModal(true)}
+          >
+            Edit Board
+          </button>
+
+          {/* show DELETE board modal button */}
+          <button
+            className="text-left text-[13px] leading-[23px] text-warning lg:hover:text-warning-hover"
+            onClick={() => setShowDeleteBoardModal(true)}
+          >
+            Delete Board
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
