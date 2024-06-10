4d81d14bfc713b788c1106de52128604c9e811ba
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-06-09 19:54:49 -0700

    Create HideSidebarButton.tsx

---
+++ app/_components/sidebar/HideSidebarButton.tsx
@@ -0,0 +1,33 @@
+import clsx from "clsx";
+
+export default function HideSidebarButton({
+  darkMode,
+  handleToggleSidebar,
+  showSidebar,
+}: {
+  darkMode: boolean;
+  handleToggleSidebar: () => void;
+  showSidebar: boolean;
+}) {
+  return (
+    <div
+      className={clsx(
+        "group/btn transition-colors md:flex md:h-12 md:w-[248px] md:items-center md:justify-start md:gap-[10px] md:rounded-r-3xl md:pl-8 lg:w-[276px] lg:hover:cursor-pointer",
+        darkMode
+          ? "md:bg-grey-dark lg:hover:bg-white"
+          : "lg:hover:bg-secondary md:bg-white",
+      )}
+      onClick={handleToggleSidebar}
+    >
+      <p
+        className={clsx(
+          showSidebar
+            ? "text-grey-medium lg:group-hover/btn:text-primary"
+            : "hidden",
+        )}
+      >
+        Hide Sidebar
+      </p>
+    </div>
+  );
+}
