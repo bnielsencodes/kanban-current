f32229ed922f2a8c021a01760e6ba79499cd39d2
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-06-09 19:42:19 -0700

    Create side effects

--- app/_components/controls/user/AvatarButton.tsx
+++ app/_components/controls/user/AvatarButton.tsx
@@ -17,6 +17,37 @@ const AvatarButton: FC<AccountPage> = ({
   const [avatar_url, setAvatarUrl] = useState<string | null>(null);
   const [avatarUrl, setAvatarUrl2] = useState<string | null>(null);
 
+  useEffect(() => {
+    let ignore = false;
+    async function getProfile() {
+      const { user } = session;
+
+      const { data, error } = await supabase
+        .from("profiles")
+        .select(`avatar_url`)
+        .eq("id", user.id)
+        .single();
+
+      if (!ignore) {
+        if (error) {
+          console.warn(error);
+        } else if (data) {
+          setAvatarUrl(data.avatar_url);
+        }
+      }
+    }
+
+    getProfile();
+
+    return () => {
+      ignore = true;
+    };
+  }, [session]);
+
+  useEffect(() => {
+    if (avatar_url) downloadImage(avatar_url);
+  }, [avatar_url]);
+
   async function downloadImage(path: string) {
     try {
       const { data, error } = await supabase.storage
